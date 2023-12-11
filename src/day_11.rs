5ad456c1311237ab9e41fda81fed84f99d0ec843
funnsam <funnsam8@gmail.com>
2023-12-11 11:55:22 +0000

    day 11 part 2

--- src/day_11.rs
+++ src/day_11.rs
@@ -46,31 +46,20 @@ pub fn part_1(file: &str) -> String {
 }
 
 pub fn part_2(file: &str) -> String {
-    // TODO: finish part 2
-    let mut g = file.lines().map(|a| a.as_bytes().to_vec()).collect::<Vec<Vec<u8>>>();
+    let g = file.lines().map(|a| a.as_bytes()).collect::<Vec<&[u8]>>();
 
     // expand y axis
-    let mut e = Vec::new();
+    let mut ey = Vec::new();
     'expand: for (yi, y) in g.iter().enumerate() {
         for x in y.iter() { if *x != b'.' { continue 'expand; } }
-        e.push(yi);
-    }
-
-    for (ei, e) in e.iter().enumerate() {
-        g.insert(ei+e, vec![b'.'; g[0].len()]);
+        ey.push(yi);
     }
 
     // expand x axis
-    let mut e = Vec::new();
+    let mut ex = Vec::new();
     'expand: for xi in 0..g[0].len() {
         for y in 0..g.len() { if g[y][xi] != b'.' { continue 'expand; } }
-        e.push(xi);
-    }
-
-    for (ei, e) in e.iter().enumerate() {
-        for y in g.iter_mut() {
-            y.insert(ei+e, b'.');
-        }
+        ex.push(xi);
     }
 
     let mut galaxies = Vec::new();
@@ -86,7 +75,9 @@ pub fn part_2(file: &str) -> String {
     for (ai, a) in galaxies.iter().enumerate() {
         for bi in 0..ai {
             let b = &galaxies[bi];
-            sum += (a.0 as isize - b.0 as isize).abs() + (a.1 as isize - b.1 as isize).abs();
+            let cx = ex.iter().filter(|c| (a.0.min(b.0)..a.0.max(b.0)).contains(c)).count();
+            let cy = ey.iter().filter(|c| (a.1.min(b.1)..a.1.max(b.1)).contains(c)).count();
+            sum += ((a.0 as isize - b.0 as isize).abs() + (a.1 as isize - b.1 as isize).abs()) as usize + (cx + cy) * 999_999;
         }
     }
 
