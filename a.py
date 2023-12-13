ead4d7166ad1de06b9ffa36d34a260723d2d6e18
funnsam <funnsam8@gmail.com>
2023-12-13 23:04:27 +0000

    day 13???

--- a.py
+++ a.py
@@ -1,28 +1,28 @@
 import os, sys
 import time
 
-# transposes a matrix represented as a list of strings
+# transposes matrix represented as list of strings
 def transpose(grid):
     new_grid = []
     for i in range(len(grid[0])):
         new_grid.append("".join([row[i] for row in grid]))
     return new_grid
 
-# finds the vertical reflection of a grid
-# by transposing the grid and finding the horizontal reflection
-def find_vertical_reflection(grid):
+# finds vertical reflection of grid
+# by transposing grid and finding horizontal reflection
+def find_vertical_reflection(grid, vertical):
     grid = transpose(grid)
-    return find_horizontal_reflection(grid)
+    return find_horizontal_reflection(grid, vertical)
 
-# finds the horizontal reflection of a grid
-def find_horizontal_reflection(grid):
+# finds horizontal reflection of grid
+def find_horizontal_reflection(grid, horizontal):
     # for each possible horizontal reflection
-    # starting from between 0, 1
     for i in range(1, len(grid)):
         is_match = True
         # for each side of the reflection
+        # starting at (i-1, i)
         for j in range(1,i+1):
-            # if right side is out of bounds, break
+            # if we reached the end of the grid, break
             if i+j-1 >= len(grid):
                 break
             # if the sides don't match, break
@@ -32,7 +32,8 @@ def find_horizontal_reflection(grid):
                 is_match = False
                 break
         # if all lines matched or we broke early due to reaching the right edge
-        if is_match:
+        # and this is not the same reflection as the one we found in part 1
+        if is_match and i != horizontal:
             # return the index of the reflection (the first index right of the reflection)
             return i
     return 0
@@ -42,7 +43,7 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
-    grids = []
+    grids: list[list[str]] = []
     current_grid = []
     # read in the grids
     for line in lines:
@@ -52,20 +53,44 @@ def main():
         else:
             current_grid.append(line)
     grids.append(current_grid)
-    
     count = 0
     # for each grid
     for grid in grids:
-        # must have either vertical or horizontal reflection
-        # never has both
-        result = find_vertical_reflection(grid)
-        # if no vertical reflection, check for horizontal reflection
-        if result == 0:
-            # must find it here
-            result = find_horizontal_reflection(grid)
-            count += 100*result
-        else:
-            count += result
+        # find original reflection
+        vertical = find_vertical_reflection(grid, 0)
+        horizontal = 0
+        if vertical == 0:
+            horizontal = find_horizontal_reflection(grid, 0)
+        
+        found_smudge = False
+        f = False
+        # for each cell in the grid
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                # flip the cell
+                new_grid = [r if y != i else row[:j] + ("#" if cell == "." else ".") + row[j+1:] for y, r in enumerate(grid)]
+                # check if the new grid has different vertical reflection
+                result = find_vertical_reflection(new_grid, vertical)
+                # if not, check if it has different horizontal reflection
+                if result == 0:
+                    result = find_horizontal_reflection(new_grid, horizontal)
+                    # if it does, count it
+                    # otherwise continue with different smudge
+                    if result != 0:
+                        count += 100*result
+                        found_smudge = True
+                        break
+                # if it does, count it
+                elif result != vertical:
+                    count += result
+                    found_smudge = True
+                    break
+            # if we found a smudge, break
+            if found_smudge:
+                f = True;
+                break
+        if not f:
+            print("h")
     print(count)
 
 if __name__ == "__main__":
--- src/day_13.rs
+++ src/day_13.rs
@@ -1,63 +1,92 @@
+fn solve(rows: &Vec<u32>, col_c: usize, orig: Option<usize>) -> Option<usize> {
+    // match vert reflection
+    let get_col = |idx: usize| {
+        rows.iter().fold(0, |a, e| (a << 1) | ((*e >> (col_c-idx-1)) & 1))
+    };
+
+    'vert: for i in 1..col_c {
+        for j in 1..i+1 {
+            if i+j-1 >= col_c {
+                break;
+            }
+
+            if get_col(i+j-1) != get_col(i-j) {
+                continue 'vert;
+            }
+        }
+
+        if let Some(orig) = orig {
+            if i != orig {
+                return Some(i);
+            }
+        } else {
+            return Some(i);
+        }
+    }
+
+    // match horiz reflection
+    'horiz: for i in 1..rows.len() {
+        for j in 1..i+1 {
+            if i+j-1 >= rows.len() {
+                break;
+            }
+
+            if rows[i+j-1] != rows[i-j] {
+                continue 'horiz;
+            }
+        }
+
+        if let Some(orig) = orig {
+            if i != orig * 100 {
+                return Some(i * 100);
+            }
+        } else {
+            return Some(i * 100);
+        }
+    }
+
+    None
+}
+
 pub fn part_1(file: &str) -> String {
     let mut total = 0;
 
-    'maps: for map in file.split("\n\n") {
+    for map in file.split("\n\n") {
         let rows = map.lines().map(|a| a.as_bytes().iter().fold(0, |a, e| (a << 1) | (*e == b'#') as u32)).collect::<Vec<u32>>();
-        let col_c = map.len() / (rows.len() + 1);
+        let col_c = map.lines().next().unwrap().len();
 
-        // match horiz reflection
-        for skip in 1..rows.len() {
-            'horiz: for idx in 0..rows.len()-1 {
-                let mut other = rows.len()-skip;
-                for j in rows.iter().skip(idx) {
-                    if other >= rows.len() || *j != rows[other] {
-                        println!("f h {idx} {skip} {j:b} {:b} {other}", rows[other.min(rows.len()-1)]);
-                        continue 'horiz;
-                    }
-                    other -= 1;
-                    if other == usize::MAX {
-                        println!("o h {idx} {skip} {j:b}");
-                        break;
-                    }
-                }
-
-                println!("d h {idx} {skip}");
-                total += (idx + rows.len()-1).div_ceil(2) * 100;
-                continue 'maps;
-            }
-        }
-
-        // match vert reflection
-        let get_col = |idx: usize| {
-            rows.iter().fold(0, |a, e| (a << 1) | ((*e >> (col_c-idx)) & 1))
-        };
-
-        for skip in 1..col_c {
-            'vert: for idx in 0..col_c {
-                let mut other = col_c-skip;
-                for jdx in idx..col_c {
-                    if get_col(jdx) != get_col(other) {
-                        println!("f v {idx} {skip} {:b} {:b} {other}", get_col(jdx), get_col(other.min(rows.len()-1)));
-                        continue 'vert;
-                    }
-
-                    other -= 1;
-                    if other == usize::MAX {
-                        println!("o v {idx} {skip} {:b}", get_col(jdx));
-                        break;
-                    }
-                }
-
-                println!("d v");
-                total += (idx - 1 + col_c).div_ceil(2);
-                continue 'maps;
-            }
-        }
+        total += solve(&rows, col_c, None).unwrap();
     }
 
     total.to_string()
 }
 
 pub fn part_2(file: &str) -> String {
-    todo!();
+    println!("This doesn't work btw");
+
+    let mut total = 0;
+
+    'maps: for map in file.split("\n\n") {
+        let rows = map.lines().map(|a| a.as_bytes().iter().fold(0, |a, e| (a << 1) | (*e == b'#') as u32)).collect::<Vec<u32>>();
+        let col_c = map.lines().next().unwrap().len();
+
+        let orig = solve(&rows, col_c, None).unwrap();
+        // smudge stuff and try until it works
+        for y in 0..rows.len() {
+            for x in 0..col_c {
+                let mut rows = rows.clone();
+                rows[y] ^= 1 << (col_c - x - 1);
+                if let Some(s) = solve(&rows, col_c, Some(orig)) {
+                    if s != orig {
+                        total += s;
+                        continue 'maps;
+                    }
+                }
+            }
+        }
+
+        println!("h");
+    }
+
+    total.to_string()
 }
